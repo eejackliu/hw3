83b937a96e3cc7bb0511b74687a9758d46cac0e1
eejackliu <pipiluliu@sina.com>
2018-11-19 18:21:36 +0800

    visual

---
+++ visual.py
@@ -0,0 +1,127 @@
+import torch
+import torchvision
+import torchvision.transforms as T
+import random
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter1d
+import matplotlib.pyplot as plt
+from cs231n.image_utils import SQUEEZENET_MEAN, SQUEEZENET_STD
+from PIL import Image
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+def preprocess(img, size=224):
+    transform = T.Compose([
+        T.Resize(size),
+        T.ToTensor(),
+        T.Normalize(mean=SQUEEZENET_MEAN.tolist(),
+                    std=SQUEEZENET_STD.tolist()),
+        T.Lambda(lambda x: x[None]),
+    ])
+    return transform(img)
+
+def deprocess(img, should_rescale=True):
+    transform = T.Compose([
+        T.Lambda(lambda x: x[0]),
+        T.Normalize(mean=[0, 0, 0], std=(1.0 / SQUEEZENET_STD).tolist()),
+        T.Normalize(mean=(-SQUEEZENET_MEAN).tolist(), std=[1, 1, 1]),
+        T.Lambda(rescale) if should_rescale else T.Lambda(lambda x: x),
+        T.ToPILImage(),
+    ])
+    return transform(img)
+def rescale(x):
+    low, high = x.min(), x.max()
+    x_rescaled = (x - low) / (high - low)
+    return x_rescaled
+
+def blur_image(X, sigma=1):
+    X_np = X.cpu().clone().numpy()
+    X_np = gaussian_filter1d(X_np, sigma, axis=2)
+    X_np = gaussian_filter1d(X_np, sigma, axis=3)
+    X.copy_(torch.Tensor(X_np).type_as(X))
+    return X
+model = torchvision.models.squeezenet1_1(pretrained=True)
+import torch.nn.functional as F
+# We don't want to train the model, so tell PyTorch not to compute gradients
+# with respect to model parameters.
+for param in model.parameters():
+    param.requires_grad = False
+from cs231n.data_utils import load_imagenet_val
+X, y, class_names = load_imagenet_val(num=5)
+
+# plt.figure(figsize=(12, 6))
+# for i in range(5):
+#     plt.subplot(1, 5, i + 1)
+#     plt.imshow(X[i])
+#     plt.title(class_names[y[i]])
+#     plt.axis('off')
+# plt.gcf().tight_layout()
+# Example of using gather to select one entry from each row in PyTorch
+# def gather_example():
+#     N, C = 4, 5
+#     s = torch.randn(N, C)
+#     y = torch.LongTensor([1, 2, 1, 3])
+#     print(s)
+#     print(y)
+#     print(s.gather(1, y.view(-1, 1)).squeeze())
+# gather_example()
+
+def compute_saliency_maps(X, y, model):
+    """
+    Compute a class saliency map using the model for images X and labels y.
+
+    Input:
+    - X: Input images; Tensor of shape (N, 3, H, W)
+    - y: Labels for X; LongTensor of shape (N,)
+    - model: A pretrained CNN that will be used to compute the saliency map.
+
+    Returns:
+    - saliency: A Tensor of shape (N, H, W) giving the saliency maps for the input
+    images.
+    """
+    # Make sure the model is in "test" mode
+    model.eval()
+     # Make input tensor require gradient
+    X.requires_grad_()
+
+    saliency = None
+    ##############################################################################
+    # TODO: Implement this function. Perform a forward and backward pass through #
+    # the model to compute the gradient of the correct class score with respect  #
+    # to each input image. You first want to compute the loss over the correct   #
+    # scores (we'll combine losses across a batch by summing), and then compute  #
+    # the gradients with a backward pass.                                        #
+    ##############################################################################
+    out=model(X)
+    loss=F.cross_entropy(out,y)
+    loss.backward()
+
+    ##############################################################################
+    #                             END OF YOUR CODE                               #
+    ##############################################################################
+    return saliency
+def show_saliency_maps(X, y):
+    # Convert X and y from numpy arrays to Torch Tensors
+    X_tensor = torch.cat([preprocess(Image.fromarray(x)) for x in X], dim=0)
+    y_tensor = torch.LongTensor(y)
+
+    # Compute saliency maps for images in X
+    saliency = compute_saliency_maps(X_tensor, y_tensor, model)
+
+    # Convert the saliency map from Torch Tensor to numpy array and show images
+    # and saliency maps together.
+    saliency = saliency.numpy()
+    N = X.shape[0]
+    for i in range(N):
+        plt.subplot(2, N, i + 1)
+        plt.imshow(X[i])
+        plt.axis('off')
+        plt.title(class_names[y[i]])
+        plt.subplot(2, N, N + i + 1)
+        plt.imshow(saliency[i], cmap=plt.cm.hot)
+        plt.axis('off')
+        plt.gcf().set_size_inches(12, 5)
+    plt.show()
+
+show_saliency_maps(X, y)
