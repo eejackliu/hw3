a355122dbaa9f51d999567f872421f5d9738bbd2
eejackliu <pipiluliu@sina.com>
2018-11-26 18:12:05 +0800

    fooling

--- visual.py
+++ visual.py
@@ -80,11 +80,11 @@ def compute_saliency_maps(X, y, model):
     - saliency: A Tensor of shape (N, H, W) giving the saliency maps for the input
     images.
     """
+
     # Make sure the model is in "test" mode
     model.eval()
      # Make input tensor require gradient
     X.requires_grad_()
-
     saliency = None
     ##############################################################################
     # TODO: Implement this function. Perform a forward and backward pass through #
@@ -127,6 +127,124 @@ def show_saliency_maps(X, y):
         plt.axis('off')
         plt.gcf().set_size_inches(12, 5)
     plt.show()
+# show_saliency_maps(X, y)
+def gradient_ascent_saliency_maps(X, y):
+    # Convert X and y from numpy arrays to Torch Tensors
+    X_tensor = torch.cat([preprocess(Image.fromarray(x)) for x in X], dim=0)
+    y_tensor = torch.LongTensor(y)
+    for i in range(200):
+    # Compute saliency maps for images in X
+        saliency = compute_saliency_maps(X_tensor, y_tensor, model)
+        X_tensor=X_tensor.detach()+saliency.unsqueeze(1)
+    # Convert the saliency map from Torch Tensor to numpy array and show images
+    # and saliency maps together.
+    saliency = saliency.numpy()
+    X_tensor=[np.asarray(deprocess(x.unsqueeze(0),should_rescale=False)) for x in X_tensor]
+    # xx=[x for x in X_tensor]
+    # print (xx[1])
+    # X_tensor=deprocess(X_tensor,should_rescale=False)
+    N = X.shape[0]
+    for i in range(N):
+        plt.subplot(2, N, i + 1)
+        plt.imshow(X[i])
+        plt.axis('off')
+        plt.title(class_names[y[i]])
+        plt.subplot(2, N, N + i + 1)
+        # plt.imshow(saliency[i], cmap=plt.cm.hot)
+        plt.imshow(X_tensor[i])
+        plt.axis('off')
+        plt.gcf().set_size_inches(12, 5)
+    plt.show()
+# gradient_ascent_saliency_maps(X,y)
+#not true the negative gradient will be positive because abs(),which would not amplify the picture
+def make_fooling_image(X, target_y, model):
+    """
+    Generate a fooling image that is close to X, but that the model classifies
+    as target_y.
 
-show_saliency_maps(X, y)
-np.maximum
+    Inputs:
+    - X: Input image; Tensor of shape (1, 3, 224, 224)
+    - target_y: An integer in the range [0, 1000)
+    - model: A pretrained CNN
+
+    Returns:
+    - X_fooling: An image that is close to X, but that is classifed as target_y
+    by the model.
+    """
+    # Initialize our fooling image to the input image, and make it require gradient
+    X_fooling = X.clone()
+    X_fooling = X_fooling.requires_grad_()
+
+    learning_rate = 1
+    ##############################################################################
+    # TODO: Generate a fooling image X_fooling that the model will classify as   #
+    # the class target_y. You should perform gradient ascent on the score of the #
+    # target class, stopping when the model is fooled.                           #
+    # When computing an update step, first normalize the gradient:               #
+    #   dX = learning_rate * g / ||g||_2                                         #
+    #                                                                            #
+    # You should write a training loop.                                          #
+    #                                                                            #
+    # HINT: For most examples, you should be able to generate a fooling image    #
+    # in fewer than 100 iterations of gradient ascent.                           #
+    # You can print your progress over iterations to check your algorithm.       #
+    ##############################################################################
+    model.eval()
+    for i in range(50):
+        out=model(X_fooling)
+        loss=F.cross_entropy(out,torch.LongTensor([target_y]))
+        # loss=out[0][target_y]
+        loss.backward()
+        # we can use both loss=out[0][target_y] and cross_entroy ,the different is that the former one is that we want the value to be maximum ,the other one we want the value to become minimum
+        # so the first we need to ascent the gradient ,the last we need to descent the gradient .Hence we need to maximum the fooling class score and minimum the crose-entropy between the input and the fooling class
+        # with torch.no_grad():
+        #     dx=learning_rate*X_fooling.grad/(X_fooling.norm()**2)
+        #     X_fooling+=dx
+        g=X_fooling.grad
+        tmp=learning_rate*g/g.norm()**2
+        X_fooling=X_fooling-tmp
+        X_fooling=X_fooling.detach()
+        X_fooling.requires_grad_()
+        print(loss)
+        if loss<1:
+            break
+    ##############################################################################
+    #                             END OF YOUR CODE                               #
+    ##############################################################################
+    return X_fooling
+idx = 0
+target_y = 6
+
+X_tensor = torch.cat([preprocess(Image.fromarray(x)) for x in X], dim=0)
+X_fooling = make_fooling_image(X_tensor[idx:idx+1], target_y, model)
+
+scores = model(X_fooling)
+assert target_y == scores.data.max(1)[1][0].item(), 'The model is not fooled!'
+X_fooling_np = deprocess(X_fooling.clone())
+X_fooling_np = np.asarray(X_fooling_np).astype(np.uint8)
+
+plt.subplot(1, 4, 1)
+plt.imshow(X[idx])
+plt.title(class_names[y[idx]])
+plt.axis('off')
+
+plt.subplot(1, 4, 2)
+plt.imshow(X_fooling_np)
+plt.title(class_names[target_y])
+plt.axis('off')
+
+plt.subplot(1, 4, 3)
+X_pre = preprocess(Image.fromarray(X[idx]))
+diff = np.asarray(deprocess(X_fooling - X_pre, should_rescale=False))
+plt.imshow(diff)
+plt.title('Difference')
+plt.axis('off')
+
+plt.subplot(1, 4, 4)
+diff = np.asarray(deprocess(10 * (X_fooling - X_pre), should_rescale=False))
+plt.imshow(diff)
+plt.title('Magnified difference (10x)')
+plt.axis('off')
+
+plt.gcf().set_size_inches(12, 5)
+plt.show()
