c8869195d1da3599fef253af506f16a1418b5eab
eejackliu <pipiluliu@sina.com>
2018-11-28 19:05:02 +0800

    finish

--- style.py
+++ style.py
@@ -29,6 +29,7 @@ def deprocess(img):
         T.Lambda(rescale),
         T.ToPILImage(),
     ])
+    return transform(img)
 def rescale(x):
     low, high = x.min(), x.max()
     x_rescaled = (x - low) / (high - low)
@@ -92,4 +93,246 @@ def extract_features(x, cnn):
     return features
 
 #please disregard warnings about initialization
-tmp=features_from_img('styles/muse.jpg',(224,224))
+def content_loss(content_weight, content_current, content_original):
+    """
+    Compute the content loss for style transfer.
+
+    Inputs:
+    - content_weight: Scalar giving the weighting for the content loss.
+    - content_current: features of the current image; this is a PyTorch Tensor of shape
+      (1, C_l, H_l, W_l).
+    - content_target: features of the content image, Tensor with shape (1, C_l, H_l, W_l).
+
+    Returns:
+    - scalar content loss
+    """
+    # content_current=content_current.view(content_current.size()[1],-1)
+    # content_original=content_original.view(content_original.size()[1],-1)
+    loss=content_weight*(content_current-content_original).norm()**2
+    return loss
+def content_loss_test(correct):
+    content_image = 'styles/tubingen.jpg'
+    image_size =  192
+    content_layer = 3
+    content_weight = 6e-2
+
+    c_feats, content_img_var = features_from_img(content_image, image_size)
+
+    bad_img = torch.zeros(*content_img_var.data.size()).type(dtype)
+    feats = extract_features(bad_img, cnn)
+
+    student_output = content_loss(content_weight, c_feats[content_layer], feats[content_layer]).cpu().data.numpy()
+    error = rel_error(correct, student_output)
+    print('Maximum error is {:.8f}'.format(error))
+
+# content_loss_test(answers['cl_out'])
+def gram_matrix(features, normalize=True):
+    """
+    Compute the Gram matrix from features.
+
+    Inputs:
+    - features: PyTorch Tensor of shape (N, C, H, W) giving features for
+      a batch of N images.
+    - normalize: optional, whether to normalize the Gram matrix
+        If True, divide the Gram matrix by the number of neurons (H * W * C)
+
+    Returns:
+    - gram: PyTorch Tensor of shape (N, C, C) giving the
+      (optionally normalized) Gram matrices for the N input images.
+    """
+    N,C,H,W=features.size()
+    features=features.view(N,C,H*W)
+    tmp=features.transpose(1,2)
+    gram=torch.bmm(features,tmp)
+    if normalize:
+        gram=gram/(C*H*W)
+    return gram
+def gram_matrix_test(correct):
+    style_image = 'styles/starry_night.jpg'
+    style_size = 192
+    feats, _ = features_from_img(style_image, style_size)
+    student_output = gram_matrix(feats[5].clone()).cpu().data.numpy()
+    error = rel_error(correct, student_output)
+    print('Maximum error is {:.8f}'.format(error))
+
+# gram_matrix_test(answers['gm_out'])
+
+from functools import reduce
+# Now put it together in the style_loss function...
+def style_loss(feats, style_layers, style_targets, style_weights):
+    """
+    Computes the style loss at a set of layers.
+
+    Inputs:
+    - feats: list of the features at every layer of the current image, as produced by
+      the extract_features function.
+    - style_layers: List of layer indices into feats giving the layers to include in the
+      style loss.
+    - style_targets: List of the same length as style_layers, where style_targets[i] is
+      a PyTorch Tensor giving the Gram matrix of the source style image computed at
+      layer style_layers[i].
+    - style_weights: List of the same length as style_layers, where style_weights[i]
+      is a scalar giving the weight for the style loss at layer style_layers[i].
+
+    Returns:
+    - style_loss: A PyTorch Tensor holding a scalar giving the style loss.
+    """
+    # Hint: you can do this with one for loop over the style layers, and should
+    # not be very much code (~5 lines). You will need to use your gram_matrix function.
+    out=[]
+    for i,index in enumerate(style_layers):
+        tmp= style_weights[i]*(gram_matrix(feats[index])-style_targets[i]).norm()**2
+        out.append(tmp)
+    return reduce(lambda x,y:x+y,out)
+def style_loss_test(correct):
+    content_image = 'styles/tubingen.jpg'
+    style_image = 'styles/starry_night.jpg'
+    image_size =  192
+    style_size = 192
+    style_layers = [1, 4, 6, 7]
+    style_weights = [300000, 1000, 15, 3]
+
+    c_feats, _ = features_from_img(content_image, image_size)
+    feats, _ = features_from_img(style_image, style_size)
+    style_targets = []
+    for idx in style_layers:
+        style_targets.append(gram_matrix(feats[idx].clone()))
+
+    student_output = style_loss(c_feats, style_layers, style_targets, style_weights).cpu().data.numpy()
+    error = rel_error(correct, student_output)
+    print('Error is {:.8f}'.format(error))
+
+
+# style_loss_test(answers['sl_out'])
+def tv_loss(img, tv_weight):
+    """
+    Compute total variation loss.
+
+    Inputs:
+    - img: PyTorch Variable of shape (1, 3, H, W) holding an input image.
+    - tv_weight: Scalar giving the weight w_t to use for the TV loss.
+
+    Returns:
+    - loss: PyTorch Variable holding a scalar giving the total variation loss
+      for img weighted by tv_weight.
+    """
+    # Your implementation should be vectorized and not require any loops!
+    # this like jitter but should not transfer the recurrent the right to left
+    left=img[:,:,:,:1]
+    right=img[:,:,:,:-1]
+    tmp_w=torch.cat([left,right],dim=3)
+    top=img[:,:,:1,:]
+    down=img[:,:,:-1,:]
+    tmp_h=torch.cat([top,down],dim=2)
+    ltv=tv_weight*((img-tmp_w).norm()**2+(img-tmp_h).norm()**2)
+    return ltv
+def tv_loss_test(correct):
+    content_image = 'styles/tubingen.jpg'
+    image_size =  192
+    tv_weight = 2e-2
+
+    content_img = preprocess(PIL.Image.open(content_image), size=image_size)
+
+    student_output = tv_loss(content_img, tv_weight).cpu().data.numpy()
+    error = rel_error(correct, student_output)
+    print('Error is {:.8f}'.format(error))
+
+# tv_loss_test(answers['tv_out'])
+def style_transfer(content_image, style_image, image_size, style_size, content_layer, content_weight,
+                   style_layers, style_weights, tv_weight, init_random = False):
+    """
+    Run style transfer!
+
+    Inputs:
+    - content_image: filename of content image
+    - style_image: filename of style image
+    - image_size: size of smallest image dimension (used for content loss and generated image)
+    - style_size: size of smallest style image dimension
+    - content_layer: layer to use for content loss
+    - content_weight: weighting on content loss
+    - style_layers: list of layers to use for style loss
+    - style_weights: list of weights to use for each layer in style_layers
+    - tv_weight: weight of total variation regularization term
+    - init_random: initialize the starting image to uniform random noise
+    """
+    # Extract features for the content image
+    content_img = preprocess(PIL.Image.open(content_image), size=image_size)
+    feats = extract_features(content_img, cnn)
+    content_target = feats[content_layer].clone()
+
+    # Extract features for the style image
+    style_img = preprocess(PIL.Image.open(style_image), size=style_size)
+    feats = extract_features(style_img, cnn)
+    style_targets = []
+    for idx in style_layers:
+        style_targets.append(gram_matrix(feats[idx].clone()))
+
+    # Initialize output image to content image or nois
+    if init_random:
+        img = torch.Tensor(content_img.size()).uniform_(0, 1).type(dtype)
+    else:
+        img = content_img.clone().type(dtype)
+    img.requires_grad_()
+
+    # Set up optimization hyperparameters
+    initial_lr = 3.0
+    decayed_lr = 0.1
+    decay_lr_at = 180
+
+    # Note that we are optimizing the pixel values of the image by passing
+    # in the img Torch tensor, whose requires_grad flag is set to True
+    optimizer = torch.optim.Adam([img], lr=initial_lr)
+
+    f, axarr = plt.subplots(1,2)
+    axarr[0].axis('off')
+    axarr[1].axis('off')
+    axarr[0].set_title('Content Source Img.')
+    axarr[1].set_title('Style Source Img.')
+    axarr[0].imshow(deprocess(content_img.cpu()))
+    axarr[1].imshow(deprocess(style_img.cpu()))
+    plt.show()
+    plt.figure()
+
+    for t in range(200):
+        if t < 190:
+            img.data.clamp_(-1.5, 1.5)
+        optimizer.zero_grad()
+
+        feats = extract_features(img, cnn)
+
+        # Compute loss
+        c_loss = content_loss(content_weight, feats[content_layer], content_target)
+        s_loss = style_loss(feats, style_layers, style_targets, style_weights)
+        # t_loss = tv_loss(img, tv_weight)
+        t_loss=0
+        loss = c_loss + s_loss + t_loss
+
+        loss.backward()
+        # Perform gradient descents on our image values
+        if t == decay_lr_at:
+            optimizer = torch.optim.Adam([img], lr=decayed_lr)
+        optimizer.step()
+
+        if t % 100 == 0:
+            print('Iteration {}'.format(t))
+            plt.axis('off')
+            plt.imshow(deprocess(img.data.cpu()))
+            plt.show()
+    print('Iteration {}'.format(t))
+    plt.axis('off')
+    plt.imshow(deprocess(img.data.cpu()))
+    plt.show()
+# Composition VII + Tubingen
+params1 = {
+    'content_image' : 'styles/tubingen.jpg',
+    'style_image' : 'styles/composition_vii.jpg',
+    'image_size' : 192,
+    'style_size' : 512,
+    'content_layer' : 3,
+    'content_weight' : 5e-2,
+    'style_layers' : (1, 4, 6, 7),
+    'style_weights' : (20000, 500, 12, 1),
+    'tv_weight' : 5e-2
+}
+
+style_transfer(**params1)
